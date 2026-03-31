2b501f82420c8d50e7cb37da286e62264d0d1325
agustindamian258-hue <agustindamian258@gmail.com>
2026-03-30 21:48:37 -0300

    Update firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,1 +1,16 @@
 
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "TU_KEY",
+  authDomain: "TU_DOMINIO",
+  projectId: "TU_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
