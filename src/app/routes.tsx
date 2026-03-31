ac181191756354c09f7a65a9d6ce6e869bff53fe
agustindamian258-hue <agustindamian258@gmail.com>
2026-03-30 22:46:21 -0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,1 +1,19 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Jobs from "../pages/Jobs";
+import Companies from "../pages/Companies";
+import CVBuilder from "../pages/CVBuilder";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/cv" element={<CVBuilder />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
