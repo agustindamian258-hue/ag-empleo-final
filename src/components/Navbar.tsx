a0062cb92a485b4e02442ea7d25172b40d1c3b53
agustindamian258-hue <agustindamian258@gmail.com>
2026-03-30 22:51:47 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,13 @@
 
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ display: "flex", gap: "10px" }}>
+      <Link to="/">Inicio</Link>
+      <Link to="/jobs">Empleos</Link>
+      <Link to="/companies">Empresas</Link>
+      <Link to="/cv">Crear CV</Link>
+    </nav>
+  );
+}
