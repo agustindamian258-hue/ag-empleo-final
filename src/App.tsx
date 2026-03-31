7b0dddf255c026063a284ed00cbb0d0de3bb6124
agustindamian258-hue <agustindamian258@gmail.com>
2026-03-31 00:24:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import AppRoutes from "./app/routes";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
-  return <AppRoutes />;
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBG0WZu-uyC1EOBk0-j2dCp8cRY3SCaiuA",
+  authDomain: "ag-empleo-final1.firebaseapp.com",
+  projectId: "ag-empleo-final1",
+  storageBucket: "ag-empleo-final1.firebasestorage.app",
+  messagingSenderId: "906358738062",
+  appId: "1:906358738062:web:b9afdb2c96f0b835418ddf",
+  measurementId: "G-M5G5ED0YC6"
+};
 
-export default App;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
