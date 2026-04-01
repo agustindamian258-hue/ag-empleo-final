f1f39ff3b10b2562ae2d459cc41ee7b23b728be0
agustindamian258-hue <agustindamian258@gmail.com>
2026-04-01 14:22:54 -0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,18 +1,21 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Jobs from "../pages/Jobs";
 import Companies from "../pages/Companies";
 import CVBuilder from "../pages/CVBuilder";
+import Feed from "../pages/Feed"; // Importamos el Feed real que armamos
+import Login from "../pages/Login"; // Importamos el Login
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} /> {/* Ruta de Login agregada */}
         <Route path="/jobs" element={<Jobs />} />
         <Route path="/companies" element={<Companies />} />
-        <Route path="/cv" element={<CVBuilder />} />
+        <Route path="/cv" element={<CVBuilder />} /> {/* Tu creador de CV con las 4 plantillas */}
+        <Route path="/social" element={<Feed />} /> {/* Ruta para el muro de Likes y Comentarios */}
       </Routes>
     </BrowserRouter>
   );
