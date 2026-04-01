7049ed5abaaf6fb7b71da890fbf8669f59a43d0c
agustindamian258-hue <agustindamian258@gmail.com>
2026-04-01 14:27:30 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,34 @@
-
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ display: "flex", gap: "10px" }}>
-      <Link to="/">Inicio</Link>
-      <Link to="/jobs">Empleos</Link>
-      <Link to="/companies">Empresas</Link>
-      <Link to="/cv">Crear CV</Link>
+    <nav style={{ 
+      display: "flex", 
+      gap: "15px", 
+      padding: "10px", 
+      background: "#f8f9fa", 
+      borderBottom: "1px solid #ddd",
+      alignItems: "center"
+    }}>
+      <Link to="/" style={{ textDecoration: "none", color: "blue" }}>Inicio</Link>
+      <Link to="/jobs" style={{ textDecoration: "none", color: "blue" }}>Empleos</Link>
+      <Link to="/companies" style={{ textDecoration: "none", color: "blue" }}>Empresas</Link>
+      <Link to="/cv" style={{ textDecoration: "none", color: "blue" }}>Crear CV</Link>
+      
+      {/* Agregamos el acceso al Feed Social que configuramos en Firebase */}
+      <Link to="/social" style={{ textDecoration: "none", color: "blue" }}>Social</Link>
+
+      {/* Agregamos el enlace de Login que pediste */}
+      <Link to="/login" style={{ 
+        marginLeft: "auto", 
+        padding: "5px 15px", 
+        background: "#007bff", 
+        color: "white", 
+        borderRadius: "5px", 
+        textDecoration: "none" 
+      }}>
+        Login
+      </Link>
     </nav>
   );
 }
