969aebff7a970103152d0147e1dc33a117c24239
agustindamian258-hue <agustindamian258@gmail.com>
2026-04-04 14:11:42 -0300

    Create Feed.tsx

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-black text-blue-600">Muro de Changas</h2>
+      <p className="text-gray-600">Aquí verás los trabajos cerca tuyo.</p>
+    </div>
+  );
+};
+
+export default Feed;
