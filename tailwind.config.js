017de32c54143577b3b4ccedd2bbc3033ae095f8
agustindamian258-hue <agustindamian258@gmail.com>
2026-04-16 00:35:23 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+// tailwind.config.js  (raíz del proyecto)
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './index.html',
     './src/**/*.{ts,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
