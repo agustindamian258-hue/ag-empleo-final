3f324f6afb0c105eebc54383a30bcd922ef676a4
agustindamian258-hue <agustindamian258@gmail.com>
2026-04-30 22:23:17 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-
+// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { analytics, logEvent } from './app/firebase'
+
+// Registrar apertura de la app
+logEvent(analytics, 'app_open');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
